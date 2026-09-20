5200c6f2702bd1166cd99388e9c0a6a0d4307a77
Raihan Sharif <raihansharif92@gmail.com>
2026-09-20 22:15:43 +0100

    post new message

--- chat-app/backend/app.js
+++ chat-app/backend/app.js
@@ -1,13 +1,25 @@
 import express from "express";
+
+import { addMessage, getAllMessages } from "./model.js";
 const app = express();
 
 // TODO: later replace with env variable
 const PORT = 3000;
 
 app.use(express.json());
+app.use(express.urlencoded());
 
 app.get("/", (req, res) => {
-    res.json({ user: "Jason" });
+    res.json(getAllMessages());
+});
+
+app.post("/", (req, res) => {
+    try {
+        addMessage(req.body.username, req.body.msg_body);
+    } catch (e) {
+        res.send(`could not send: ${e.message}`);
+    }
+    res.redirect("/");
 });
 
 app.listen(PORT, () => {
