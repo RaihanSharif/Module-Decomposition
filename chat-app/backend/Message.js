be3e08fc450c7a95998ef03314476f4a7f67b177
Raihan Sharif <raihansharif92@gmail.com>
2026-09-22 14:06:25 +0100

    Define message class with autoincrement id

---
+++ chat-app/backend/Message.js
@@ -0,0 +1,25 @@
+export class Message {
+    static #nextId = 0;
+
+    /**
+     *
+     * @param {string} username the message sender
+     * @param {*} msg_body text content of the message
+     */
+    constructor(username, msg_body, likes = 0, dislikes = 0) {
+        if (!username) {
+            throw new Error("username is required");
+        }
+
+        if (!msg_body) {
+            throw new Error("msg_body is required");
+        }
+
+        this.id = Message.#nextId++;
+        this.username = username;
+        this.msg_body = msg_body;
+        this.createdAt = new Date();
+        this.likes = likes;
+        this.dislikes = dislikes;
+    }
+}
